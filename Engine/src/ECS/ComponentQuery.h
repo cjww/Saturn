c4040fdb133c810301777db8fea9af71b3bccfee
Julius Wendt <julius.wendt@live.se>
2021-05-24 22:40:54 +0200

    added option to query required components and others seperatly

--- Engine/src/ECS/ComponentQuery.h
+++ Engine/src/ECS/ComponentQuery.h
@@ -7,11 +7,13 @@ enum class QueryOperator {
 };
 class ComponentQuery {
 private:
-	ComponentMask m_componentMask;
+	ComponentMask m_requiredComponents;
+	ComponentMask m_components;
 	QueryOperator m_op;
 public:
 	ComponentQuery();
-	ComponentQuery(ComponentMask componentMask, QueryOperator op = QueryOperator::AND);
+	ComponentQuery(ComponentMask components, QueryOperator op = QueryOperator::AND);
+	ComponentQuery(ComponentMask components, ComponentMask requiredComponents);
 
 	bool check(ComponentMask signature) const;
 
