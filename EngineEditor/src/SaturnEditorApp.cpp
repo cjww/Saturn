c61ca379fb71bfa431a767e961b03bdc3cbbe520
Julius <julius.wendt@live.se>
2026-05-03 17:32:00 +0200

    Getting shader path based on current editor path

--- EngineEditor/src/SaturnEditorApp.cpp
+++ EngineEditor/src/SaturnEditorApp.cpp
@@ -19,7 +19,7 @@ namespace sa {
 	};
 
 	Application* CreateApplication(int argc, char** argv) {
-		Engine::SetShaderDirectory("../../build/Engine/shaders");
+		Engine::SetShaderDirectory(std::filesystem::current_path() / "../Engine/shaders");
 		return new EditorApp(argc, argv);
 	}
 
